87228e6b468eda82f8453643f1f77e49e93bdca2
Chris Haigh <chris@zolv.com>
2018-08-19 16:07:49 +0100

    Added PowerShell scripts for git getting-started.

---
+++ 04-Undo-Changes-In-Working-Copy.ps1
@@ -0,0 +1,26 @@
+# Undoing changes in the working copy
+
+# Do something accidental...
+Write-Output $null > .\readme.md
+
+git status
+
+# Revert to previous by using git checkout...
+git checkout .\readme.md
+
+# git status now shows nothing to commit...
+git status
+
+# readme.md is now as it was...
+Get-Content .\readme.md
+
+# do some buggering up...
+Write-Output $null > .\readme.md
+Remove-Item .\new_file_name.txt
+
+git status
+
+
+
+# Oops!  Time to do a hard reset...
+git reset --hard
