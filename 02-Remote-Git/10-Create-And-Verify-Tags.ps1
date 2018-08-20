b60cba81a8267f7f6f8ed050d1bfc945c4b65f94
Chris Haigh <cshaigh1981@gmail.com>
2018-08-20 20:48:01 +0100

    Tags script: tidied up script/variables

--- 02-Remote-Git/10-Create-And-Verify-Tags.ps1
+++ 02-Remote-Git/10-Create-And-Verify-Tags.ps1
@@ -15,44 +15,39 @@ git tag
 
 # Alias to gpg.exe
 $gpgCmd = "C:\Program Files\Git\usr\bin\gpg.exe"
-$keyFile = ".\gen-key.txt"
+$keyGenScriptFile = ".\gen-key.txt"
 
-# Create array to hold key file script
-$keyFileScript = @()
-
-# Wipe out contents of gen-key.txt file
-Write-Output $null > $keyFile
-
-# Get a password
+# Get a passphrase
 $response = Read-Host "What passphrase would you like to generate your key?" -AsSecureString
 $passphrase = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($response))
 
-$keyFileScript += "%echo Generating a key"
-$keyFileScript += "Key-Type: RSA"
-$keyFileScript += "Key-Length: 2048"
-$keyFileScript += "Subkey-Type: RSA"
-$keyFileScript += "Subkey-Length: 2048"
-$keyFileScript += "Passphrase: $passphrase"
-$keyFileScript += "Name-Real: Chris Haigh"
-$keyFileScript += "Name-Comment: Head of DevOps"
-$keyFileScript += "Name-Email: cshaigh1981@gmail.com"
-$keyFileScript += "Expire-Date: 1y"
+# Generate key file script
+$keyGenScript = @()
+$keyGenScript += "%echo Generating a key"
+$keyGenScript += "Key-Type: RSA"
+$keyGenScript += "Key-Length: 2048"
+$keyGenScript += "Subkey-Type: RSA"
+$keyGenScript += "Subkey-Length: 2048"
+$keyGenScript += "Passphrase: $passphrase"
+$keyGenScript += "Name-Real: Chris Haigh"
+$keyGenScript += "Name-Comment: Head of DevOps"
+$keyGenScript += "Name-Email: cshaigh1981@gmail.com"
+$keyGenScript += "Expire-Date: 1y"
 
 # Do a commit here, so that we can later print "done" :-)
-$keyFileScript += "%commit"
-$keyFileScript += "%echo done"
+$keyGenScript += "%commit"
+$keyGenScript += "%echo done"
 
-# Write $keyFileScript to $keyFile
-$keyFileScript | Out-File -Encoding ascii $keyFile
+# Write $keyGenScript to $keyGenScriptFile
+$keyGenScript | Out-File -Encoding ascii $keyGenScriptFile
 
-# Generate key using keyFileScript
-& $gpgCmd --batch --gen-key $keyFile
+# Generate key using keyGenScript
+& $gpgCmd --batch --gen-key $keyGenScriptFile
 
-# List key
-& $gpgCmd --list-key
+$key = Read-Host "What was the trusted key given above?"
 
 # Set user key to have same key as pub
-git config --global user.signingkey 92107CE4
+git config --global user.signingkey $key
 
 # Verify
 & $gpgCmd --list-key
